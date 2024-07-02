bdde674b2436ca00f3c63382bbed94f5a0c784fc
Sultana Razia <sultanarazia483@gmail.com>
2024-07-02 10:54:00 +0600

    Store Applied Jobs To Local Stoage

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,26 +1,41 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../utility/localstorage";
 
 
 const JobDetails = () => {
     const jobs = useLoaderData();
-    const { id, job_description, job_responsibility, salary } = useParams();
+    const { id, job_description, job_responsibility, educational_requirements, experiences, salary } = useParams();
     const job = jobs.find(job => job.id == id);
-    console.log(job_description);
+    console.log(job);
+
+    const handleApplyJob = () => {
+        saveJobApplication(id);
+        toast('You have applied successfully.')
+    }
+
     return (
         <div>
             <h2>Job Details of: {id}</h2>
             <div className="grid md:grid-cols-4 gap-4">
-                <div className="border md:col-span-3">
-                    <p><span>Job Description</span>{job.job_description}</p>
-                    <p>{job.job_responsibility}</p>
+                <div className="md:col-span-3">
+                    <p className="text-[#757575] font-medium mb-6"><span className="text-[#1A1919] font-extrabold">Job Description: </span>{job.job_description}</p>
+                    <p className="text-[#757575] font-medium mb-6"><span className="text-[#1A1919] font-extrabold">Job Responsibility: </span>{job.job_responsibility}</p>
+                    <p className="text-[#757575] font-medium mb-6">
+                        <span className="text-[#1A1919] font-extrabold">Educational Requirements: </span>{job.educational_requirements}</p>
+                    <p className="text-[#757575] font-medium mb-6">
+                        <span className="text-[#1A1919] font-extrabold">Experiences: </span>
+                        {job.experiences}</p>
                 </div>
                 <div className="border">
                     <p>Salary{salary}</p>
                     <div className="bg-[#7E90FE] text-center rounded-lg py-3">
-                        <button className="text-xl font-extrabold text-white ">Apply Now</button>
+                        <button onClick={handleApplyJob} className="text-xl font-extrabold text-white ">Apply Now</button>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
